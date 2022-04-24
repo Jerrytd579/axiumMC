7782e5e9e46c829bc9db229d060d6e8b046467ee
Jerry <jerrytd579@gmail.com>
2022-04-24 15:31:31 -0400

    routes to rules page, updated navbar, working on rules

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,17 @@ const path = require('path')
 
 const constructorMethod = (app) => {
 
-    app.use('/', (req, res) => {
-        res.sendFile(path.resolve('static/index.html'))
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve('static/index.html'))
     });
 
-    app.use('*', (req, res) => {
+    app.get('/rules', (req, res) =>{
+      res.sendFile(path.resolve('static/rules.html'))  
+    })
+
+    app.get('*', (req, res) => {
       res.redirect('/')
+      //res.status(404).json({error: 'Not found!'});
     });
   };
 
